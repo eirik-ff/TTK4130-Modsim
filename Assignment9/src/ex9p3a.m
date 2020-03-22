db2a84fd29b71a8ab57d0a91fb4bb8fc863a8a75
Eirik Falck <eirik.falck@gmail.com>
2020-03-22 15:03:44 +0100

    finish exercise 9

--- Assignment9/src/ex9p3a.m
+++ Assignment9/src/ex9p3a.m
@@ -11,16 +11,15 @@ z = sym('z');
 L = 1;
 g = 9.81;
 
-%FSym = dx - [zeros(3), eye(3); -z*eye(3), -g*diag([0,0,1])] * x;
 FSym = [dx; 0] - [v; -g*[0;0;1] - z*p; (p.')*dv + (v.')*v]; 
 dFdxdotSym = jacobian(FSym,dx);
 dFdxSym = jacobian(FSym,x);
 dFdzSym = jacobian(FSym,z);
 
-F = matlabFunction(FSym, 'vars', {dx,x,z,t});
+F       = matlabFunction(FSym,       'vars', {dx,x,z,t});
 dFdxdot = matlabFunction(dFdxdotSym, 'vars', {dx,x,z,t});
-dFdx = matlabFunction(dFdxSym, 'vars', {dx,x,z,t});
-dFdz = matlabFunction(dFdzSym, 'vars', {dx,x,z,t});
+dFdx    = matlabFunction(dFdxSym,    'vars', {dx,x,z,t});
+dFdz    = matlabFunction(dFdzSym,    'vars', {dx,x,z,t});
 
 x0 = [1, 0, 0, 0, 1, 0].';
 z0 = 1;
@@ -45,6 +44,9 @@ dt = 0.01; % TODO: try different
 T = 0:dt:tf;
 
 [x,xdot,z] = RKDAE(BT_IRK4, F, dFdxdot, dFdx, dFdz, T, x0, z0);
+
+%% Simulation using built-in solvers
+[tout,xout] = ode15s(@(t,x) F(x(4:6),x(1:3),x(7),t), [0 tf], [x0; z0]);
 
 
 %% Plot 3D
